b8394412c84cb7e4a248af6383b324c7117d641d
nono7129 <168652267+nono7129@users.noreply.github.com>
2024-05-26 17:41:19 +0900

    Add files via upload

--- cm4.py
+++ cm4.py
@@ -1,11 +1,14 @@
-from random import *
-a = 0
-for i in range(1, 51):
-    time = randrange(5, 51)
-    if time <= 15:
-      print("[O] {}번째 손님 (소요시간 : {})분)".format(i, time))
-      a += 1
+def std_bmr(s, weight, height, age):
+    if s == 'male':
+        return 66.47 + (13.75 * weight) + (5 * height) - (6.76 * age)
     else:
-      print("[] {}번째 손님 (소요시간 : {})분)".format(i, time))
-      a += 0
-print("총 탑승객 수 :", a)
+        return 665.1 + (9.56 * weight) + (1.85 * height) - (4.68 * age)
+    
+
+s = 'male'      
+weight = 62       
+height = 178      
+age = 20         
+
+bmr = std_bmr(s, weight, height, age)
+print(f"당신의 기초대사량은 {bmr} 입니다.")
