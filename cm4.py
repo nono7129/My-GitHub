517526550568e5b4650ad5b42cea5342e39fa01f
nono7129 <168652267+nono7129@users.noreply.github.com>
2024-05-26 17:39:46 +0900

    Add files via upload

--- cm4.py
+++ cm4.py
@@ -1,25 +1,11 @@
-Public Sub GeneratePassword()
-    Dim passwordLength As Integer
-    Dim password As String
-    Dim charset As String
-    charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+"
-
-    ' 비밀번호 길이 입력 받기
-    passwordLength = Val(InputBox("비밀번호 길이를 입력하세요:"))
-
-    ' 입력값이 숫자가 아닌 경우 오류 처리
-    If Not IsNumeric(passwordLength) Then
-        MsgBox "숫자를 입력하세요."
-        Exit Sub
-    End If
-
-    ' 비밀번호 생성
-    password = ""
-    For i = 1 To passwordLength
-        password = password & Mid(charset, Int((Len(charset) * Rnd) + 1), 1)
-    Next i
-
-    ' 생성된 비밀번호 출력
-    MsgBox "안전한 비밀번호: " & password
-End Sub
-
+from random import *
+a = 0
+for i in range(1, 51):
+    time = randrange(5, 51)
+    if time <= 15:
+      print("[O] {}번째 손님 (소요시간 : {})분)".format(i, time))
+      a += 1
+    else:
+      print("[] {}번째 손님 (소요시간 : {})분)".format(i, time))
+      a += 0
+print("총 탑승객 수 :", a)
