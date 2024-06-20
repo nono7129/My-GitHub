c60cc8f320d83d760a3a15c9b31a1e0bd2d40c24
nono7129 <168652267+nono7129@users.noreply.github.com>
2024-06-20 17:05:01 +0900

    Add files via upload

---
+++ 4p-line plot chart_2.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [2016, 2017, 2018, 2019, 2020]
+y = [350, 410, 520, 695, 543]
+plt.plot(x, y)
+plt.title('Annual sales')
+plt.xlabel('years')
+plt.ylabel('sales')
+plt.show()
